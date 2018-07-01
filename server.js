24dee8d2b53342c1b9cff14b30ed71479b5ca06b
Gianni Mereu <gmer16@inbox.ru>
2018-07-01 19:04:54 +0100

    adding database endpoint

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
+      host : 'postgresql-concentric-80107',
       user : 'gmere',
       password : '123',
       database : 'face-recognition'
