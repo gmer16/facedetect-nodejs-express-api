394d1e82c92b9631cb1410df8fe85ed3257de454
Gianni Mereu <gmer16@inbox.ru>
2018-07-01 18:19:12 +0100

    First Heroku upload

---
+++ README.md
@@ -0,0 +1,3 @@
+> First thing to do is to figure out which endpoints the app may need,
+the functionality that may be required and afterwords test it with a tool
+like postman, independently from the frontend
---
+++ controllers/image.js
@@ -0,0 +1,32 @@
+// Clarifai
+const Clarifai = require('clarifai');
+const app = new Clarifai.App({
+  apiKey: 'a591935006d6421ba35559d9ee349e5b'
+ });
+
+const handleAPICall = (req, res) => {
+    app.models // accessing the Clarifai API
+       .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+       .then(data => {
+           res.json(data);
+           console.log(data);
+       })
+       .catch(err => res.status(400).json('unable to work with API'));
+}
+
+const handleImage = (req, res, db) => {
+    const { id } = req.body;
+    db('users')
+    .where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+        res.json(entries[0]);
+    })
+    .catch(err => res.status(400).json('unable to get entries'));
+}
+
+module.exports = {
+    handleImage: handleImage,
+    handleAPICall: handleAPICall
+}
