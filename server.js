3b1110ce34349fff366498b36eff383cdfd37ed7
Gianni Mereu <gmer16@inbox.ru>
2018-07-01 19:11:27 +0100

    database conn update

--- server.js
+++ server.js
@@ -13,10 +13,8 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : 'postgresql-concentric-80107',
-      user : 'gmere',
-      password : '123',
-      database : 'face-recognition'
+      host : process.env.DATABASE_URL,
+      ssl: true
     }
   });
 
